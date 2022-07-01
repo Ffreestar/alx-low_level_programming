263c9aedf04ec2b5bbc41df42334114cd544d8a5
ffreestar <adeayooluwasegun5@gmail.com>
2022-06-30 21:38:53 -0700

    ...

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-/*
- * output somthing from the main block
- * return; 0
+/**
+ * main - main block
+ * Return: 0
  */
-
 int main(void)
 {
-put("\"Programming is like building a multilingual puzzle");
-return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return(0);
 }
